937873dcf1572fe4e45fd9d6b6fc5fbd18c85862
Leo70001 <lkamdem29@gmail.com>
2024-05-17 18:34:06 +0200

    commit 1

---
+++ src/AppliInfo.java
@@ -0,0 +1,24 @@
+public class AppliInfo {
+private String name;
+private  String version;
+private  String author;
+private String info;
+
+public AppliInfo(String name, String version, String author, String info){
+    this.name = name;
+    this.version = version;
+    this.author = author;
+    this.info = info;
+}
+
+public String getDescription(){
+    String description = null;
+    description = description.concat("===" + name + "===\n");
+    description = description.concat("Version:" +version+ "\n");
+    description = description.concat("Auteur:" + author+ "\n");
+    description = description.concat(info);
+    return  description;
+
+}
+
+}
---
+++ src/Splash.java
@@ -0,0 +1,26 @@
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class Splash extends JFrame{
+    private AppliInfo info;
+    private  JTextArea infoLbl;
+
+    public static void main(String[] args)
+    {
+        new Splash();
+    }
+
+    public Splash(){
+        info = new AppliInfo("Splash Screen","1.0.0","A.", "Une application pour débuter");
+        creerInterface();
+    }
+
+    private void  creerInterface(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setLayout(new BorderLayout());
+
+        infoLbl = new JTextArea();
+        infoLbl.setText(info.getDescription());
+    }
+}
